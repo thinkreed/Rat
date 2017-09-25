98ecb8238144e2fe3018fa83926c9596c833d13d
thinkreed <thinkreed2017@outlook.com>
2017-09-25 23:52:33 +0800

    add espresso test for main activity

--- app/src/main/java/thinkreed/rat/MainActivity.kt
+++ app/src/main/java/thinkreed/rat/MainActivity.kt
@@ -1,30 +1,34 @@
 package thinkreed.rat
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    // Example of a call to a native method
-    sample_text.text = stringFromJNI()
-  }
-
-  /**
-   * A native method that is implemented by the 'native-lib' native library,
-   * which is packaged with this application.
-   */
-  external fun stringFromJNI(): String
-
-  companion object {
-
-    // Used to load the 'native-lib' library on application startup.
-    init {
-      System.loadLibrary("native-lib")
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+        sample_text.setOnClickListener {
+            Toast.makeText(this@MainActivity, "click on text", Toast.LENGTH_SHORT).show()
+        }
     }
-  }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
 }
