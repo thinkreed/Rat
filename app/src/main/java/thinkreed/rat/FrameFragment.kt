2792c78ebf099f1284ff6a8be71cbbe4c0f402be
thinkreed <thinkreed2017@outlook.com>
2017-09-27 22:00:41 +0800

    add a frame for rat

---
+++ app/src/main/java/thinkreed/rat/FrameFragment.kt
@@ -0,0 +1,16 @@
+package thinkreed.rat
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by thinkreed on 2017/9/27.
+ */
+class FrameFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_frame, container, false)
+    }
+}
